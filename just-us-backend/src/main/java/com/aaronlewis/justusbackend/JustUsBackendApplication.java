b12038bdc6d41580b2d7ebafca6814fbcbbb91cd
Aaron Lewis <aaron11125@gmail.com>
2021-01-18 19:04:45 -0800

    Project creation

---
+++ just-us-backend/src/main/java/com/aaronlewis/justusbackend/JustUsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aaronlewis.justusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustUsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustUsBackendApplication.class, args);
+	}
+
+}
---
+++ just-us-backend/src/test/java/com/aaronlewis/justusbackend/JustUsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronlewis.justusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustUsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
